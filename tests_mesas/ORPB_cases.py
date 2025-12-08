3b1ada3e273c46fdc0fd985535069a42ca06187a
19megan <simonsen.megan@gmail.com>
2025-12-08 15:22:56 -0500

    Added a single discharge SAS spec case

--- tests_mesas/ORPB_cases.py
+++ tests_mesas/ORPB_cases.py
@@ -9,15 +9,15 @@ sas_specs_storage_q_ug_et_u = {
                  'a': 1.0,
                  'b': 1.0,
                  'loc': 0.0,
-                 'scale': 0.254
+                 'scale': 0.254 #just estimating scale when it's not a string
                  }}
         }, #if >1 dict for 'quickflow (mm/hr)', then mesas looks for column named 'ORPB qf' and other dict key(s) -- >1 dict will allow for weighted SAS functions, these columns in df will be the weights [0,1]
     'baseflow 1 (mm/hr)':
         {'baseflow 1 (mm/hr) SAS function':
                 {'func': 'gamma',
-                 'args': { 'a': 1.26,
+                 'args': { 'a': 1.26, # not exponential bc a != 1, so we estimate 'a'
                            'loc': 0,
-                           'scale':'storage (mm)' #2106.1584
+                           'scale':'storage (mm)' #2106.1584 # string so we estimate lamda and S_c
                             }}
         },
     'ET (mm/hr)': 
@@ -27,10 +27,34 @@ sas_specs_storage_q_ug_et_u = {
                  'a': 1.0,
                  'b': 1.0,
                  'loc': 0.0,
-                 'scale': 43.19
+                 'scale': 43.19 #just estimating scale when it's not a string
              }}
         }
 }
+
+sas_specs_storage_q_g_et_u = {
+    'discharge (mm/hr)':
+        {'discharge (mm/hr) SAS function':
+            {'func':'gamma',
+                'args': {
+                    'a': 1.26,
+                    'loc': 0.0,
+                    'scale': 'S_scale' #'storage (mm)'
+                    }}
+        },
+    'ET (mm/hr)':
+        {'ET (mm/hr) SAS function':
+            {'func':'kumaraswamy',
+             'args':{
+                 'a': 1.0,
+                 'b': 1.0,
+                 'loc': 0.0,
+                 'scale': 43.19 #just estimating scale when it's not a string
+             }}
+        }
+
+}
+
 #C_old = -7.6 but I'm getting errors for a negative scale in param_dist so I'll change it
 solute_parameters = {'precip 18O': {'C_old': 1, 'observations': 'ORPB 18O'}
                      }
@@ -51,7 +75,7 @@ obs_uncertainty = {
     },
     'sigma C out': { #ORPB 18O':{
         'prior_dis': 'normal',
-        'prior_params': [5.0, 5.0], #[-7.325, 0.581], # mean, std
+        'prior_params':[0.02, .02], #[5.0, 5.0], #[-7.325, 0.581], # mean, std
         'is_nonnegative': True #False
     }
 }                  
@@ -72,6 +96,14 @@ scale_parameters = {
 
 theta_storage_q_ug_et_u = {
     'sas_specs': sas_specs_storage_q_ug_et_u,
+    'solute_parameters': solute_parameters,
+    'options': options,
+    'obs_uncertainty': obs_uncertainty,
+    'scale_parameters': scale_parameters
+}
+
+theta_storage_q_g_et_u = {
+    'sas_specs': sas_specs_storage_q_g_et_u,
     'solute_parameters': solute_parameters,
     'options': options,
     'obs_uncertainty': obs_uncertainty,
