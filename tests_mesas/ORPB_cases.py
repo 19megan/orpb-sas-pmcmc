e73a6ce47657d575b156f33c51fda4162f6fa069
19megan <simonsen.megan@gmail.com>
2026-03-10 11:30:13 -0400

    Added composite SAS function and uniform SAS function and changed scale param priors

--- tests_mesas/ORPB_cases.py
+++ tests_mesas/ORPB_cases.py
@@ -1,25 +1,23 @@
 # This script will contain all of the test cases for sas_specs, solute parameters, options, obs_uncertainty, scale_parameters, and theta dictionaries for SAS model using pMCMC
 # Created on: 11/10/2025
 
-sas_specs_storage_q_ug_et_u = {
-    'quickflow (mm/hr)':
-        {'quickflow (mm/hr) SAS function':
-            {'func':'kumaraswamy', #uniform dist
+sas_specs_storage_q_gg_et_u = {
+    'discharge (mm/hr)':
+        {'qf_weight': # this column in df will be the weight for the quickflow SAS function, and 1 - this column will be the weight for the baseflow 1 SAS function:
+            {'func':'gamma',
              'args': {
-                 'a': 1.0,
-                 'b': 1.0,
+                 'a': .5,
                  'loc': 0.0,
-                 'scale': 0.254 #just estimating scale when it's not a string
-                 }}
-        }, #if >1 dict for 'quickflow (mm/hr)', then mesas looks for column named 'ORPB qf' and other dict key(s) -- >1 dict will allow for weighted SAS functions, these columns in df will be the weights [0,1]
-    'baseflow 1 (mm/hr)':
-        {'baseflow 1 (mm/hr) SAS function':
-                {'func': 'gamma',
-                 'args': { 'a': 1.26, # not exponential bc a != 1, so we estimate 'a'
-                           'loc': 0,
-                           'scale':'storage (mm)' #2106.1584 # string so we estimate lamda and S_c
-                            }}
-        },
+                 'scale': .5
+                 }},
+         'bf1_weight':
+             {'func':'gamma',
+              'args': {
+                  'a': 1.26,
+                  'loc': 0.0,
+                  'scale': 'S_scale'
+                  }}
+        },        
     'ET (mm/hr)': 
         {'ET (mm/hr) SAS function':
             {'func':'kumaraswamy',
@@ -30,6 +28,30 @@ sas_specs_storage_q_ug_et_u = {
                  'scale': 43.19 #just estimating scale when it's not a string
              }}
         }
+}
+
+sas_specs_storage_q_u_et_u = {
+    'discharge (mm/hr)':
+        {'discharge (mm/hr) SAS function':
+            {'func':'kumaraswamy', #uniform dist
+                'args': {
+                    'a': 1.0,
+                    'b': 1.0,
+                    'loc': 0.0,
+                    'scale': 'storage (mm)'
+                    }}
+        },
+    'ET (mm/hr)':
+        {'ET (mm/hr) SAS function':
+            {'func':'kumaraswamy',
+             'args':{
+                 'a': 1.0,
+                 'b': 1.0,
+                 'loc': 0.0,
+                 'scale': 43.19 #just estimating scale when it's not a string
+             }}
+        }
+
 }
 
 sas_specs_storage_q_g_et_u = {
@@ -64,17 +86,17 @@ obs_uncertainty = {
     # sig_u
     'sigma observed C in': { #precip 18O': {
         'prior_dis': 'normal',
-        'prior_params': [0.05, 0.02],#[-6.706, 3.293], # mean, std
+        'prior_params': [0.01, 0.01],#[-6.706, 3.293], # mean, std
         'is_nonnegative': True #False # sig_u becomes the scale for normal dist so can't be neg
     },
     'sigma filled C in': { #precip 18O':{
         'prior_dis': 'normal',
-        'prior_params': [1.0, 0.5], #[-6.706, 3.293], # mean, std
+        'prior_params': [0.02, 0.02], #[-6.706, 3.293], # mean, std
         'is_nonnegative': True #False
     },
     'sigma C out': { #ORPB 18O':{
         'prior_dis': 'normal',
-        'prior_params':[0.02, .02], #[5.0, 5.0], #[-7.325, 0.581], # mean, std
+        'prior_params':[0.01, .01], #[5.0, 5.0], #[-7.325, 0.581], # mean, std
         'is_nonnegative': True #False
     }
 }                  
@@ -82,19 +104,27 @@ obs_uncertainty = {
 scale_parameters = {
     'lambda':{
         'prior_dis': 'normal',
-        'prior_params': [-103.0, 10.3], # mean, std
-        'is_nonnegative': False
+        'prior_params': [.5, .2], #[11.54, 1.5],#[0.0099, 1.5], # mean, std
+        'is_nonnegative': True
     },
     'S_c':{
         'prior_dis': 'normal',
-        'prior_params': [48.0, 4.8], # mean, std
-        'is_nonnegative': True
+        'prior_params': [-131, 10], #[-320.03, 10.0], # mean, std
+        'is_nonnegative': False
     }
 }
 
 
-theta_storage_q_ug_et_u = {
-    'sas_specs': sas_specs_storage_q_ug_et_u,
+theta_storage_q_gg_et_u = {
+    'sas_specs': sas_specs_storage_q_gg_et_u,
+    'solute_parameters': solute_parameters,
+    'options': options,
+    'obs_uncertainty': obs_uncertainty,
+    'scale_parameters': scale_parameters
+}
+
+theta_storage_q_u_et_u = {
+    'sas_specs': sas_specs_storage_q_u_et_u,
     'solute_parameters': solute_parameters,
     'options': options,
     'obs_uncertainty': obs_uncertainty,
